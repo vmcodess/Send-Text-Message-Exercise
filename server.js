a1215e5f09b19c99e55cf8ef1a1d6e1f9b9dcf1a
vmatveev <vmatveevspam@gmail.com>
2020-02-01 17:36:53 -0500

    Twilio works, add your own Twilio SID an authToken in the server.js

--- server.js
+++ server.js
@@ -55,7 +55,7 @@ app.post("/sendText", (req,res)=>{
         const accountSID = process.env.accountSid;
         const authToken = process.env.authToken;
 
-        const client = require('twilio')(process.env.accountSID, process.env.authToken);
+        const client = require('twilio')(accountSID, authToken);
 
         client.messages
         .create({
